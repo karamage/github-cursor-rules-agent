247e3cdac5b146fa8ed44c224d9b97e4f250378f
karamage <karamage.umauma@gmail.com>
2025-12-05 16:02:21 +0900

    feat: add Vercel deployer to Mastra configuration and update dependencies

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,6 +2,7 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
+import { VercelDeployer }  from '@mastra/deployer-vercel';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
 import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
@@ -16,6 +17,7 @@ export const mastra = new Mastra({
     url: ":memory:",
   }),
   */
+  deployer: new VercelDeployer(),
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
